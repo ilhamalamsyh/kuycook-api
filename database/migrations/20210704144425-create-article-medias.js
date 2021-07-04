0d1cfbc169c8b9ea9d2b5b4c3335c5c42bf633bd
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2021-07-04 21:47:41 +0700

    add article media models & its migrations

---
+++ database/migrations/20210704144425-create-article-medias.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('article_medias', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      articleId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('article_medias');
+  }
+};
---
+++ database/models/article_medias.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { DataTypes } = require("sequelize/types");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+  const ArticleMedia = sequelize.define(
+    'ArticleMedia',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER.UNSIGNED
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      articleId: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        field: 'created_at'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        field: 'updated_at'
+      },
+      deletedAt: {
+        allowNull: true,
+        type: DataTypes.DATE,
+        field: 'deleted_at'
+      },
+    },
+    {
+      tableName: 'article_medias'
+    }
+  )
+  return ArticleMedia;
+}
