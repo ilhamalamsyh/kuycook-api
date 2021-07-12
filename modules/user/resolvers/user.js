d345db34cba19bc250a0cd646cf0d7a0f3ac4c23
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2021-07-12 22:06:10 +0700

    add resolver for user

--- modules/user/resolvers/user.js
+++ modules/user/resolvers/user.js
@@ -1,45 +1,75 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { AuthenticationError } = require('apollo-server-express');
-import { ApolloError } from 'apollo-server-errors';
-
+const { ApolloError } = require('apollo-server-errors');
 
 const { User } = require('../../../database/models');
+const user = require('../schemas/user');
 
 module.exports = {
     Mutation:{
         register: async (root, args, context) => {
-            const {name, email, password, gender} = args.input;
-            return User.create({name, email, password, gender});
+            //TODO:Add validation of registration
+            const {fullname, email, password, gender} = args.input;
+            return User.create({fullname, email, password : await bcrypt.hash(password, 10), gender});
         },
 
         login: async (root, { input }, context) => {
+            //TODO:Add validation of login
             const { email, password} = input;
             const user = await User.findOne({where: {email}});
+            console.log(user.password);
+
             if (user && bcrypt.compareSync(password, user.password)) {
                 const token = jwt.sign({ id: user.id }, 'mysecret', {expiresIn:'1d'});
-                return {...user.toJson(), token};
+                return { ...user.toJSON(), token};
             }
+            console.log(user.password);
+            console.log(password);
             throw new AuthenticationError('Invalid Credential');
         },
 
-        updateUser: async (root, context, {
-            id,
-            name,
-            email,
-            password,
-            gender
-        }) => {
+        // updateUser: async (root, { id, input }, context) => {
+        //     const { fullname, password, email, gender } = input;
+        //     console.log(id);
+        //     try {
+        //         // if (!user) {
+        //         //     throw new AuthenticationError('You must login to update a user');
+        //         // }
+        //         return await User && User.update({
+        //             fullname,
+        //             email,
+        //             password,
+        //             gender
+        //         }, {where: {id: id}});
+        //         console.log(`UserUpdate: ${user}`);
+                
+        //     } catch (error) {
+        //         throw new Error(`something went wrong: ${error}`)
+        //     }
+        // }
+        
+        userUpdate: async (parent, { id, input }, context) => {
+            //TODO:Add validation of updateUser
             try {
-                const user = await User && User.update({
-                    name,
+                const {fullname, email, password, gender} = input;
+                const user = await User.findByPk(id);
+                console.log(user);
+                if (!user) {
+                    throw new ApolloError(
+                        'User not found'
+                      );
+                }
+
+                const userUpdated =  await user.update({
+                    fullname,
                     email,
-                    password,
+                    password : await bcrypt.hash(password, 10),
                     gender
-                }, {where: {id: id}});
-                return {...user.toJson()}
+                });
+                return userUpdated;
             } catch (error) {
-                throw new ApolloError('Error Update User', 'MY_ERROR_CODE', error);
+                throw new Error(`something went wrong: ${error}`)
             }
         }
     },
