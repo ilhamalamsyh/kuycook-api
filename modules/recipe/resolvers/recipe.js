9f3cf3c097e425021204fbc4010ab428325c507b
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2021-07-21 13:07:38 +0700

    add query(recipeList, recipeDetail), add recipe schema

---
+++ modules/recipe/resolvers/recipe.js
@@ -0,0 +1,106 @@
+const { User, Recipe,  RecipeIngredient, RecipeInstruction, RecipeMedia} = require('../../../database/models');
+
+const { AuthenticationError, UserInputError } = require('apollo-server-express');
+
+//TODO: bikin transaction create using "unmanaged transaction" dengan beberapa model yg dibutuhkan
+
+module.exports = {
+    Query: {
+        getAllRecipes: async (root, args, { user }) => {
+            try {
+                const userId = user.id;
+                return Recipe.findAll(
+                    {
+                        logging: console.log,
+                        include:[
+                        {
+                            model: User,
+                            as: 'User',
+                            where: {id: userId}
+                        },
+                        {
+                            model: RecipeIngredient,
+                            as: 'ingredients',
+                            required: true,
+                        },{
+                            model: RecipeInstruction,
+                            as: 'instructions',
+                            require:true
+                        },{
+                            model: RecipeMedia,
+                            as:'image',
+                            require: true
+                        }]
+                    });
+            } catch (error) {
+                throw new Error();
+            }
+            
+        },
+
+        getRecipeDetail: async (_, { id }, { user }) => {
+            try {
+                userId = user.id;
+                return Recipe.findByPk(id,{
+                    include: [
+                    {
+                        model: User,
+                        as: 'User',
+                        where: {id: userId}
+                    },{
+                        model: RecipeIngredient,
+                        as: 'ingredients',
+                        required: true,
+                    },{
+                        model: RecipeInstruction,
+                        as: 'instructions',
+                        require:true
+                    },{
+                        model: RecipeMedia,
+                        as:'image',
+                        require: true
+                    }]
+                });   
+            } catch (error) {
+                throw new Error();
+            }
+        }
+    },
+
+
+}
+
+
+/// will use it when we have different naming between schema and model
+
+// try {
+//     const t = await Recipe.findAll(
+//     {
+//         logging:console.log,
+//         include:[
+//         {
+//             model: RecipeIngredient,
+//             as: 'recipe_ingredient',
+//             required: true
+//         },
+//         {
+//             model: RecipeInstruction,
+//             as: 'recipe_instruction',
+//             required: true
+//         }]
+//     }); 
+    
+//     console.log(t);
+    
+    // Mapping
+//     return t.map(item => {
+//         const newItem = item.get({plain:true});
+//         newItem.ingredients = newItem.recipe_ingredient
+
+//         newItem.instructions = newItem.recipe_instruction
+//         console.log(newItem);
+//         return newItem;
+//     });
+// } catch (error) {
+//     console.log('Error nih: ', error);
+// }
---
+++ modules/recipe/schemas/recipe.js
@@ -0,0 +1,79 @@
+const { gql } = require('apollo-server-express');
+
+module.exports = gql`
+    scalar Data
+
+    type Recipe {
+        id: ID!
+        title: String
+        description: String
+        isFavorite: Boolean
+        instructions: [RecipeInstruction]!
+        ingredients: [RecipeIngredient]!
+        image: RecipeMedia!
+        createdAt: Date
+        updatedAt: Date
+        deletedAt: Date
+    }
+
+    extend type Mutation {
+        recipeCreate(input: RecipeInput!): Recipe
+        recipeUpdate(id: ID!, input: RecipeInput!): Recipe
+        recipeDelete(id: ID!): Recipe
+    }
+
+    extend type Query {
+        getAllRecipes: [Recipe]
+        getRecipeDetail(id: ID!): Recipe
+    }
+
+    type RecipeInstruction {
+        id: ID!
+        instruction: String
+        createdAt: Date
+        updatedAt: Date
+        deletedAt: Date
+        recipeId: Int!
+    }
+
+    type RecipeIngredient {
+        id: ID!
+        ingredient: String
+        createdAt: Date
+        updatedAt: Date
+        deletedAt: Date
+        recipeId: Int!
+    }
+
+    type RecipeMedia {
+        id: ID!
+        url: String
+        recipeId: Int!
+    }
+
+    type FavoriteRecipe {
+        id: ID!
+        userId: Int!
+        recipeId: Int!
+    }
+
+    input RecipeInput {
+        title: String!
+        description: String!
+        ingredient: [RecipeIngredientInput!]!
+        instruction: [RecipeInstructionInput!]!
+        image: RecipeMediaInput
+    }
+
+    input RecipeInstructionInput {
+        instruction: String!
+    }
+
+    input RecipeIngredientInput {
+        ingredient: String!
+    }
+
+    input RecipeMediaInput {
+        url: String!
+    }
+` 
