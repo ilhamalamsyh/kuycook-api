80c9af680282c0903cad07bc45d78ba5fcf08a06
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2021-07-21 13:14:01 +0700

    add recipe resolver exporting

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,3 +1,4 @@
 const userResolver = require('../../modules/user/resolvers/user');
+const recipeResolver = require('../../modules/recipe/resolvers/recipe');
 
-module.exports = [userResolver];
+module.exports = [userResolver, recipeResolver];
