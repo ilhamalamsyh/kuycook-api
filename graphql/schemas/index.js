54ce7c4042865d263e5d9dd81fb23ffc79693799
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2021-07-21 13:17:12 +0700

    add recipe schema exporting

--- graphql/schemas/index.js
+++ graphql/schemas/index.js
@@ -1,5 +1,6 @@
 const { gql } = require('apollo-server-express');
 const userType = require('../../modules/user/schemas/user');
+const recipeType = require('../../modules/recipe/schemas/recipe');
 
 const rootType = gql`
     type Query{
@@ -11,4 +12,4 @@ const rootType = gql`
     }
 `;
 
-module.exports = [rootType, userType];
+module.exports = [rootType, userType, recipeType];
