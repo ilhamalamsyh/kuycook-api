18ba30eb959e7296ca8bf59d4d0ba3bc8aa830cc
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2021-09-30 16:17:47 +0700

    feature(validation): add recipe update form validation

--- middleware/fields/recipeInputFieldsValidation.js
+++ middleware/fields/recipeInputFieldsValidation.js
@@ -37,4 +37,49 @@ const recipeFormValidation = (
   return schema.validate(title, description, ingredients, instructions, image);
 };
 
-module.exports = { recipeFormValidation };
+const recipeUpdateFormValidation = (
+  title,
+  description,
+  ingredients,
+  instructions,
+  image
+) => {
+  const schema = Joi.object()
+    .keys({
+      title: Joi.string().trim().min(3).required().messages({
+        'string.empty': `Please input the title`,
+        'string.min': `Title is at least 3 characters`,
+      }),
+      description: Joi.string().trim().min(5).required().messages({
+        'string.empty': `Please input the description`,
+        'string.min': `Description is at least 5 characters`,
+      }),
+      ingredients: Joi.array().items(
+        Joi.object().keys({
+          id: Joi.optional(),
+          recipeId: Joi.string().optional(),
+          ingredient: Joi.string().trim().min(3).required().messages({
+            'string.empty': `Please input the ingredient`,
+            'string.min': `Ingredient is at least 3 characters`,
+          }),
+        })
+      ),
+      instructions: Joi.array().items(
+        Joi.object().keys({
+          id: Joi.optional(),
+          recipeId: Joi.string().optional(),
+          instruction: Joi.string().trim().min(5).required().messages({
+            'string.empty': `Please input the instruction`,
+            'string.min': `Instruction is at least 5 characters`,
+          }),
+        })
+      ),
+      image: Joi.string().trim().required().messages({
+        'string.empty': `Image is required`,
+      }),
+    })
+    .options({ abortEarly: false });
+  return schema.validate(title, description, ingredients, instructions, image);
+};
+
+module.exports = { recipeFormValidation, recipeUpdateFormValidation };
