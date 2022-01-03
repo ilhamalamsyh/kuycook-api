1bb748c26725dd325d350e6094ac7ca2153ab099
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2022-01-03 10:24:58 +0700

    add birthdate field in user model

--- database/models/user.js
+++ database/models/user.js
@@ -6,77 +6,82 @@ const Sequelize = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
-    {
-      id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      fullname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      gender: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-        field: 'created_at',
-        defaultValue: Sequelize.NOW,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-        field: 'updated_at',
-        defaultValue: Sequelize.NOW,
-      },
-      deletedAt: {
-        allowNull: true,
-        type: DataTypes.DATE,
-        field: 'deleted_at',
-      },
-    },
-    {
-      tableName: 'users',
-      defaultScope: {
-        rawAttributes: { exclude: ['password'] },
-      },
-    }
-  );
+    const User = sequelize.define(
+        'User',
+        {
+            id: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            fullname: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            gender: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            birthDate: {
+                allowNull: false,
+                type: DataTypes.DATE,
+                field: 'birth_date'
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+                field: 'created_at',
+                defaultValue: Sequelize.NOW,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+                field: 'updated_at',
+                defaultValue: Sequelize.NOW,
+            },
+            deletedAt: {
+                allowNull: true,
+                type: DataTypes.DATE,
+                field: 'deleted_at',
+            },
+        },
+        {
+            tableName: 'users',
+            defaultScope: {
+                rawAttributes: {exclude: ['password']},
+            },
+        }
+    );
 
-  // Added beforeCreate hook which automatically hashes the password using bcrypt.js under the hood
-  User.beforeCreate(async (user) => {
-    // eslint-disable-next-line no-param-reassign
-    user = await user.generatedPasswordHash();
-  });
-
-  // eslint-disable-next-line func-names
-  // eslint-disable-next-line consistent-return
-  User.prototype.generatedPasswordHash = function () {
-    if (this.password) {
-      return bcrypt.hash(this.password, 10);
-    }
-  };
-
-  // define relationship a user has many recipes
-  User.associate = function (models) {
-    User.hasMany(models.Recipe, {
-      foreignKey: 'userId',
-      as: 'recipe',
+    // Added beforeCreate hook which automatically hashes the password using bcrypt.js under the hood
+    User.beforeCreate(async (user) => {
+        // eslint-disable-next-line no-param-reassign
+        user = await user.generatedPasswordHash();
     });
-  };
-  return User;
+
+    // eslint-disable-next-line func-names
+    // eslint-disable-next-line consistent-return
+    User.prototype.generatedPasswordHash = function () {
+        if (this.password) {
+            return bcrypt.hash(this.password, 10);
+        }
+    };
+
+    // define relationship a user has many recipes
+    User.associate = function (models) {
+        User.hasMany(models.Recipe, {
+            foreignKey: 'userId',
+            as: 'recipe',
+        });
+    };
+    return User;
 };
