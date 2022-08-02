b503394c64a3f48332a21851c9dbe74ee971ec81
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2022-08-02 20:23:34 +0700

    feat: add user update validation

--- middleware/fields/userInputFieldsValidation.js
+++ middleware/fields/userInputFieldsValidation.js
@@ -33,4 +33,32 @@ const userFormValidation = (fullname, email, password, gender) => {
     return schema.validate(fullname, email, password, gender);
 };
 
-module.exports = {userFormValidation};
+const userUpdateFormValidation = (fullname, email, gender) => {
+    const schema = Joi.object()
+        .keys({
+            fullname: Joi.string().trim().min(8).required().messages({
+                'string.base': `"Fullname" should be a text type`,
+                'string.empty': `Enter Fullname`,
+                'string.min': `Use at least 8 characters`,
+                'any.required': `"fullname" is required`,
+            }),
+            email: Joi.string()
+                .trim()
+                .email({minDomainSegments: 2, tlds: ['com', 'net']})
+                .required()
+                .messages({
+                    'string.base': `"Email" should be a text type`,
+                    'string.email': `Email format should be ".com" or ".net"`,
+                    'string.empty': `Enter Email`,
+                }),
+            gender: Joi.string().trim().max(1).required().messages({
+                'string.empty': `Select Gender`,
+                'string.max': 'Select Gender',
+            }),
+            birthDate: Joi.date().max('now').min('1947-01-01'),
+        })
+        .options({abortEarly: false});
+    return schema.validate(fullname, email, gender);
+};
+
+module.exports = { userFormValidation, userUpdateFormValidation };
