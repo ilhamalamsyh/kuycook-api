b9bcfafcae0e34aefeb0c52f96a6de4f1239c213
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2022-08-30 20:52:15 +0700

    change env to production

--- database/models/index.js
+++ database/models/index.js
@@ -12,7 +12,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 
 // const config = require(__dirname + '/../config/config.js')[env];
 const config = require('../config/config')[env];
