dae0a6984023f0371381dc304312c4d6778209d6
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2022-08-30 18:46:21 +0700

    refactor: change production env

--- database/config/config.js
+++ database/config/config.js
@@ -17,11 +17,11 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: process.env.DEV_DB_USERNAME,
-    password: process.env.DEV_DB_PASS,
-    database: process.env.DEV_DB_NAME,
-    host: process.env.DEV_DB_HOST,
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASS,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
     dialect: 'postgres',
-    use_env_variable: process.env.DEV_DATABASE_URL,
+    use_env_variable: process.env.DATABASE_URL,
   },
 };
