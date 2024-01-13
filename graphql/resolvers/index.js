106f02b45eee32e763613c76cf596dc429cd9a93
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2024-01-13 11:12:40 +0700

    chore(resolvers): add upload resolver to pool resolvers

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,10 +2,12 @@ const userResolver = require('../../modules/user/resolvers/user');
 const recipeResolver = require('../../modules/recipe/resolvers/recipe');
 const articleResolver = require('../../modules/article/resolvers/article');
 const bannerResolver = require('../../modules/banner/resolvers/banner');
+const uploadFileResolver = require('../../modules/upload-file/resolvers/upload');
 
 module.exports = [
   userResolver,
   recipeResolver,
   articleResolver,
   bannerResolver,
+  uploadFileResolver
 ];
